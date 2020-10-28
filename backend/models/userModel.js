1306b08720ccb7439b9698ac1b4a075725f65413
martinmiralles <martinmiralles@gmail.com>
2020-10-28 15:51:32 -0400

    Prepared sample data and used bcryptjs to hash passwords

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,7 +15,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
-    password: {
+    isAdmin: {
       type: Boolean,
       required: true,
       default: false,
