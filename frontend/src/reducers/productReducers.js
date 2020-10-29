8fbd9b2e7f3d814e7b126e4e7f507aaf6f61e115
martinmiralles <martinmiralles@gmail.com>
2020-10-29 13:01:28 -0400

    Created Product Details constants, reducers, added to store, created action, and used the action with useEffect

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,7 +1,11 @@
+// REDUX WORKFLOW STEP 2: Import Constants here, and create Reducer functions, then add them to 'store.js'
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from "../constants/productConstants.js";
 
 // Reducers take 2 things: (1) An initial state, and (2) An action
@@ -13,6 +17,22 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { product: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,17 @@
+// REDUX WORKFLOW STEP 3: Import reducer methods, then add them to the 'combineReducers' method arguments (as a key-value pair),
+//then create a relevant action in the 'actions' folder
+
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { productListReducer } from "./reducers/productReducers.js";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers.js";
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const initialState = {};
