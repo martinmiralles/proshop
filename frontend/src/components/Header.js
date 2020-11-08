832a9a0181d2315d8caf8d340b743f12401b535e
martinmiralles <martinmiralles@gmail.com>
2020-11-08 14:22:01 -0500

    Added access security to the admin screen

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -50,13 +50,13 @@ const Header = () => {
               )}
               {userInfo && userInfo.isAdmin && (
                 <NavDropdown title="Admin" id="adminmenu">
-                  <LinkContainer to="admin/userList">
+                  <LinkContainer to="/admin/userList">
                     <NavDropdown.Item>Users</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to="admin/productList">
+                  <LinkContainer to="/admin/productList">
                     <NavDropdown.Item>Products</NavDropdown.Item>
                   </LinkContainer>
-                  <LinkContainer to="admin/orderList">
+                  <LinkContainer to="/admin/orderList">
                     <NavDropdown.Item>Orders</NavDropdown.Item>
                   </LinkContainer>
                 </NavDropdown>
