18f78de103ac2d0c1d7ec06ddb24fc896573c7d9
martinmiralles <martinmiralles@gmail.com>
2020-11-08 13:47:03 -0500

    Created relevant front end files for admin user list as well as the screen and navbar link

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -14,6 +14,7 @@ import {
   userRegisterReducer,
   userDetailsReducer,
   userUpdateProfileReducer,
+  userListReducer,
 } from "./reducers/userReducers.js";
 import {
   orderCreateReducer,
@@ -34,6 +35,7 @@ const reducer = combineReducers({
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
   orderListMy: orderListMyReducer,
+  userList: userListReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
